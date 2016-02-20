462eca31182ccb6ebf6fc385d7f1183ba4467518
Brian Mancini <bmancini@gmail.com>
2016-02-20 09:12:10 -0500

    Added comments to quicktest on how the code currently works

--- quicktest/file.js
+++ quicktest/file.js
@@ -1,34 +1,55 @@
 /**
- * This represents an extraction service that is scalable
+ * This represents a simple file service that listens for incoming
+ * requests for files and responds with the file bytes. This service
+ * is an example of a leaf-level service that other services will
+ * rely on.
  */
 
 let fs = require('fs');
 let path = require('path');
-let bluebird = require('bluebird');
 let amqplib = require('amqplib');
-let brokerPath = 'amqp://192.168.99.100';
-
+let bluebird = require('bluebird');
 bluebird.promisifyAll(fs);
 
+let brokerPath = process.env.BROKER_PATH || 'amqp://192.168.99.100';
+
+
+/**
+ * This code contains some boilerplate that can be abstracted into an Application object
+ * where mounts are attached, similar to Express.
+ *
+ * For now, it contains all of the setup logic to bootstrap the service.
+ */
 async function start() {
   let rabbit = await amqplib.connect(brokerPath);
   let channel = await rabbit.createChannel();
+  console.log('Connected to RabbitMQ');
 
-  // create listening queues
+  // asserts that the following queues are available
   await channel.assertQueue('file.read', { durable: true });
   await channel.assertQueue('file.read.complete', { durable: false });
 
+  // Begins consuming the queue using the "frameworks" handler method.
+  // This will be converted into a middleware function so that multiple methods
+  // can be bound to consumed messages.
   channel.consume('file.read', (msg) => handle('file.read', channel, msg, onFile).catch(err => console.log(err.stack)));
 }
 
+// Executes the start method and logs any exceptions. Similar to an Express app.listen(() => {});
 start().catch(err => console.log(err.stack));
 
 
 /**
- * Meta programming to auto ack requests
+ * Handler method for all inbound events that are being watched.  This method
+ * is bound to a specific event signature and has a processing lambda that
+ * can return a result. This method will auto-ack via a new event that appends '.complete'
+ * to the event name.
+ *
+ * IF the processing lambda returns a result, that result is sent back as the ACK.
+ * IF there is no result from the processing lambda, an empty object is returned.
  */
-async function handle(event, channel, msg, handler) {
-  let result = await handler(channel, msg);
+async function handle(event, channel, msg, processMsg) {
+  let result = await processMsg(channel, msg);
   let buffer = result;
   let correlationId = msg.properties.correlationId;
   if(result) {
@@ -37,7 +58,7 @@ async function handle(event, channel, msg, handler) {
     }
   }
   else {
-    buffer = new Buffer('ok');
+    buffer = new Buffer({});
   }
   channel.sendToQueue(event + '.complete', buffer, { correlationId });
   channel.ack(msg);
@@ -45,7 +66,13 @@ async function handle(event, channel, msg, handler) {
 
 
 /**
- * Send file bytes
+ * Finally an implemention of a mounted method.  This method is mounted to the 'file.read' event.
+ * It's fairly simple, all it does it take the fileId as input and return the file bytes as a
+ * return function.
+ *
+ * Ideally... this should be a pure function.  Some refactor may be done here so that the
+ * "metadata" such as correlationId and channel are passed in as properties on arguments object.
+ * This would allow destructuring operations to apply to the method arguments.
  */
 async function onFile(channel, msg) {
   let correlationId = msg.properties.correlationId;
--- quicktest/gateway.js
+++ quicktest/gateway.js
@@ -1,43 +1,82 @@
 /**
- * This represents how a monolith would interact with the
- * the MS infrastructure. A request is made and events
- * are triggered that are aggregated by the aggregator
+ * This represents an API Gateway where REST is used to interact with the system.
+ * To me, solving this interaction piece is important since it dictates how
+ * an existing monolith would interact with an event-based system.
  */
 
 let express = require('express');
-let bodyParser = require('body-parser');
-
 let amqp = require('amqplib');
-let brokerPath = 'amqp://192.168.99.100';
+let uuid = require('node-uuid');
 
+let brokerPath = process.env.BROKER_PATH || 'amqp://192.168.99.100';
 
+/**
+ * This code contains some boilerplate that can be abstracted into an Application object
+ * where mounts are attached, similar to Express.
+ *
+ * For now, it contains all of the setup logic to bootstrap the service.
+ */
 async function start() {
   let broker = await amqp.connect(brokerPath);
   let channel = await broker.createChannel();
   console.log('Connected to RabbitMQ');
 
-  // create a callback channel
-  let replyToQueue = await channel.assertQueue('', {exclusive:true });
-  let replyTo = replyToQueue.queue;
-
-  // ensure interactive queues are alive
+  // asserts that the following queues are available
   await channel.assertQueue('file.read', { durable: true });
   await channel.assertQueue('file.read.complete', { durable: false });
-  await channel.assertQueue('extract.file', { durable: true });
 
+  // bootstrap the express application
   let app = express();
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.get('/file', (req, res, next) => file(req, res).catch(next));
-  app.post('/extract', (req, res, next) => extract(req, res).catch(next));
-  app.get('/extract', (req, res, next) => extract(req, res).catch(next));
-  app.get('/status', (req, res, next) => status(req, res).catch(next));
+  app.get('/file', (req, res, next) => getFile(req, res).catch(next));
+
+
+  /**
+   * Express mount method implemented with async/await.
+   * It is mounted to /file and requires a 'fileId' querystring property.
+   */
+  async function getFile(req, res, next) {
+    let fileId = req.query.fileId;
+    console.log('Requesting %s', fileId);
+
+    if(!fileId)
+      res.status(400).send('fileId is required');
+
+    let result = await publish('file.read', fileId);
+    res.send(result);
+  }
+
+  // start the express application
   app.listen(5050, () => console.log('Express running on 5050'));
 
-  app.get('/file', (req, res, next) => file(req, res).catch(next));
 
+  // internel property on the application object for storing the
+  // correlationId:callback back... explained below
   let callbacks = {}
 
+  /**
+   * Framework method for publishing data into a queue.  This method does a few things
+   * that will be managed by the farmework:
+   *
+   * 1. Starts a consumer channel on the queue.name.complete queue that looks for
+   *    correlationIds in the callback object. This is necessary because the consumer,
+   *    once started will blindly listen for any messages in the completion queue
+   *    and we wnat to filter to just the ones that match our correlationId... this
+   *    may be an inefficiency (but was designed to allow pubsub) like behavior.
+   *    We MAY be able to get around it by naming the callback event:
+   *    queue.name.complete.correlationId so that this service can be directly bound
+   *    to only handle messages it cares about.  I'm not sure if the binding/unbinding
+   *    overhead would perform better though.  Something to test. I digress.
+   *
+   * 2. The data is emitted onto the queue with the new correlationId
+   * 3. A new callback is added to the callback lookup with the correlation Id. This callback
+   *    is a resolve
+   *
+   * This returns a promise
+   *
+   */
   function publish(queue, data) {
+    // Consume on the completion change and execute the corresponding
+    // callback if it matches our correlationId.
     channel.consume(queue + '.complete', (msg) => {
       let correlationId = msg.properties.correlationId;
       console.log('Received message %s', correlationId);
@@ -46,50 +85,23 @@ async function start() {
       }
     }, { noAck: true });
 
+    // Return a promise that will resolve when the callback is executed. This
+    // allows for usage of async/await by the consumer!
     return new Promise((resolve) => {
-      let correlationId = generateUuid();
+      // create correlationId
+      let correlationId = uuid.v4();
+
+      // emit to queue. This will be converted to emit to the worker exchange
+      // based on the proposed architecture.
       channel.sendToQueue(queue, new Buffer(data), { correlationId });
+
+      // Create a callback lambda that resolves the current promise with the
+      // results of the message!
       callbacks[correlationId] = (msg) => resolve(msg.content.toString());
     });
   }
-
-
-  async function file(req, res, next) {
-    let fileId = req.query.fileId;
-    console.log('Requesting %s', fileId);
-    let result = await publish('file.read', fileId);
-    res.send(result);
-  }
-
-  // async function extract(req, res, next) {
-  //   let correlationId = generateUuid();
-  //   let fileIds = req.body.fileIds || req.query.fileIds;
-  //   for(let fileId of fileIds) {
-  //     console.log('Sending file %s', fileId);
-  //     await channel.sendToQueue('extract.file', new Buffer(fileId), { correlationId });
-  //   }
-  //   res.send({ status: 'ok' });
-  // }
-
-  // async function status(req, res, next) {
-  //   let correlationId = generateUuid();
-  //   let fileId = req.query.fileId;
-  //   await channel.sendToQueue('extract.status', new Buffer(fileId), { correlationId, replyTo });
-  //   await channel.consume(replyTo, (msg) => {
-  //     if(msg.properties.correlationId === correlationId) {
-  //       res.send(msg.content.toString());
-  //     }
-  //   }, {noAck: true});
-  // }
 }
 
+// Executes the start method and logs any exceptions. Similar to an Express app.listen(() => {});
 start().catch(err => console.log(err.stack));
 
-
-
-
-function generateUuid() {
-  return Math.random().toString() +
-         Math.random().toString() +
-         Math.random().toString();
-}
