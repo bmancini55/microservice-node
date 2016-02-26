d186fbc597234dcabf432e65fe182d76cb307c69
Brian Mancini <bmancini@gmail.com>
2016-02-26 17:11:20 -0500

    Removed Request library

--- extraction/file-service.js
+++ extraction/file-service.js
@@ -2,7 +2,7 @@
 const BROKER_PATH = process.argv[2] || process.env.BROKER_PATH;
 
 let fs = require('fs');
-let request = require('request');
+let http = require('http');
 let framework = require('./app')({ name: 'file' });
 
 framework.start(BROKER_PATH).catch(console.log);
@@ -18,9 +18,39 @@ async function fileBytes(path) {
 
 function getFile(path) {
   return new Promise((resolve, reject) => {
-    request.get(path, (err, response, body) => {
-      if(err) reject(err);
-      else resolve(body);
-    })
+
+    http.get(path, (res) => {
+      let contentLength = res.headers['content-length'];
+      let contentType = res.headers['content-type'];
+      let buffers = []
+      res.on('data', (chunk) => {
+        buffers.push(chunk);
+      });
+      res.on('end', () => {
+        let buffer = Buffer.concat(buffers);
+        let result = {
+          contentType: contentType,
+          body: buffer.toString('base64')
+        };
+        console.log(' [x] %d %d %s', contentLength, buffer.length, contentType);
+        resolve(JSON.stringify(result));
+      })
+    }).on('error', reject);
+
+
+    // request.get(path, (err, response, body) => {
+    //   if(err) reject(err);
+    //   else {
+    //     let result = {
+    //       contentType: response.headers['content-type'],
+    //       body: body
+    //     };
+    //     console.log(response.)
+    //     console.log( '[x] %d', response.headers['content-length']);
+    //     console.log(' [x] %d %s', result.body.length, result.contentType);
+    //     result = JSON.stringify(result);
+    //     resolve(result);
+    //   }
+    // })
   });
 }
--- extraction/text-service.js
+++ extraction/text-service.js
@@ -2,38 +2,60 @@
 const BROKER_PATH = process.argv[2] || process.env.BROKER_PATH;
 const TIKA_PATH = process.argv[3] || process.env.TIKA_PATH;
 
-let fs = require('fs');
-let path = require('path');
+let url = require('url');
+let http = require('http');
 let uuid = require('node-uuid');
-let request = require('request');
 let framework = require('./app')({ name: 'text' });
 
 framework.start(BROKER_PATH).catch(console.log);
 framework.on('text.extract', extractText);
 
 async function extractText(path, { publish }) {
-  let byteString = await publish('file.bytes', path);
-  let result = await callTika(byteString);
+  let str = await publish('file.bytes', path);
+  let file = JSON.parse(str);
+  file.body = new Buffer(file.body, 'base64');
+  let result = await callTika(file);
   return result;
 }
 
 
 // HELPERS
 
-async function bufferToStream(buffer) {
-  let stream = new require('stream').Duplex();
-  stream.push(buffer);
-  stream.push(null);
-  return stream;
-}
-
-async function callTika(byteString) {
-  let byteStream = await bufferToStream(byteString);
+async function callTika(file) {
   return new Promise((resolve, reject) => {
-    byteStream
-      .pipe(request.put(TIKA_PATH + ':9998/tika', (err, response, body) => {
-        if(err) reject(err);
-        else resolve(body);
-      }))
+    console.log(' [x] %d %s', file.body.length, file.contentType);
+    let tikaPath = url.parse(TIKA_PATH);
+    let req = http.request({
+      hostname: tikaPath.hostname,
+      port: 9998,
+      path: '/tika',
+      method: 'PUT',
+      headers: {
+        'Content-Type': file.contentType
+      }
+    }, (res) => {
+      let buffers = [];
+      res.on('data', (buffer) => buffers.push(buffer));
+      res.on('end', () => {
+        let result = Buffer.concat(buffers);
+        resolve(result);
+      });
+    });
+    req.on('error', reject);
+    req.write(file.body);
+    req.end();
+    // request.put({
+    //   url: TIKA_PATH + ':9998/tika',
+    //   body: file.body,
+    //   headers: {
+    //     'Content-type': file.contentType
+    //   }
+    // }, (err, response, body) => {
+    //   console.log(' [x] Status code %s', response.statusCode);
+    //   if(err) reject(err);
+    //   else {
+    //     resolve(body);
+    //   }
+    // });
   });
 }
