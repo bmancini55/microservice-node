7fd80c3d5db4869334166f3a53697a60544dece3
Brian Mancini <bmancini@gmail.com>
2016-03-02 19:49:27 -0500

    Support local file system

--- extraction/file-service.js
+++ extraction/file-service.js
@@ -13,11 +13,32 @@ framework.on('file.bytes', fileBytes);
 
 async function fileBytes(path) {
   if(!path) throw new Error('Path is required');
-  let bytes = await getFile(path);
-  return bytes;
+
+  if(path.startsWith('http') || path.startsWith('https'))
+    return await getUrl(path);
+  else
+    return await getFile(path);
 }
 
-function getFile(path) {
+async function getFile(path) {
+  console.log(' [x] file read of %s', path);
+  let contentType = await getContentType(path);
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, buffer) => {
+      if (err) reject(err);
+      else {
+        let result = {
+          contentType: contentType,
+          body: buffer.toString('base64')
+        };
+        console.log(' [x] %d %s', buffer.length, contentType);
+        resolve(result);
+      }
+    });
+  });
+}
+
+async function getUrl(path) {
   return new Promise((resolve, reject) => {
     let agent = path.startsWith('https') ? https : http;
     agent.get(path, (res) => {
@@ -38,4 +59,22 @@ function getFile(path) {
       })
     }).on('error', reject);
   });
+}
+
+async function getContentType(path) {
+  let parts = path.split('.');
+  let ext = parts[parts.length - 1].toLowerCase();
+  console.log(' [x] extension %s', ext);
+  switch(ext) {
+    case 'mht': return 'message/rfc822';
+    case 'pdf': return 'application/pdf';
+    case 'dat': return 'application/octet-stream';
+    case 'dbf': return 'application/octet-stream';
+    case 'doc': return 'application/msword';
+    case 'xls': return 'application/vnd.ms-excel';
+    case 'rtf': return 'application/rtf';
+    case 'gif': return 'image/gif';
+    case 'txt': return 'text/plain';
+    default: return 'text/plain';
+  }
 }
